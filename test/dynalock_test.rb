80b59ef4bf15f40ef19f3bacfa1d1d3fb8a12052
Daniel Amireh <daniel@amireh.org>
2018-03-02 10:00:01 +0100

    Fix typo
    adquire -> acquire

--- test/dynalock_test.rb
+++ test/dynalock_test.rb
@@ -10,18 +10,18 @@ class DynalockTest < Minitest::Test
   TABLE="test"
 
 
-  def test_adquire_lock
+  def test_acquire_lock
     ctx = "test_1"
     delete(ctx)
     assert_nil get(ctx), "Environment should be clean"
-    assert_equal true, adquire_lock(table: TABLE, owner: @owner, context: ctx, expire_time: 1), "Can't adquire lock"
+    assert_equal true, acquire_lock(table: TABLE, owner: @owner, context: ctx, expire_time: 1), "Can't acquire lock"
     refute_nil get(ctx), "Lock should exists"
 
-    assert_equal false, adquire_lock(table: TABLE, owner: @owner, context: ctx, expire_time: 1), "Was able to adquire lock while being lock"
+    assert_equal false, acquire_lock(table: TABLE, owner: @owner, context: ctx, expire_time: 1), "Was able to acquire lock while being lock"
 
     sleep 2
 
-    assert_equal true, adquire_lock(table: TABLE, owner: @owner, context: ctx, expire_time: 1), "Lock is should not exists"
+    assert_equal true, acquire_lock(table: TABLE, owner: @owner, context: ctx, expire_time: 1), "Lock is should not exists"
   ensure
     delete(ctx)
   end
@@ -30,17 +30,17 @@ class DynalockTest < Minitest::Test
     ctx = "test_2"
     delete(ctx)
 
-    assert_equal true, adquire_lock(table: TABLE, owner: @owner, context: ctx, expire_time: 1), "We should be able to pick the token"
-    assert_equal false, adquire_lock(table: TABLE, owner: @owner, context: ctx, expire_time: 1), "Lock should exists ASDFASDFASDF"
+    assert_equal true, acquire_lock(table: TABLE, owner: @owner, context: ctx, expire_time: 1), "We should be able to pick the token"
+    assert_equal false, acquire_lock(table: TABLE, owner: @owner, context: ctx, expire_time: 1), "Lock should exists ASDFASDFASDF"
     assert_equal true, refresh_lock(table: TABLE, owner: @owner, context: ctx, expire_time: 3), "Token should be refresh"
 
     sleep 2
 
-    assert_equal false, adquire_lock(table: TABLE, owner: @owner, context: ctx, expire_time: 1), "Lock should exists"
+    assert_equal false, acquire_lock(table: TABLE, owner: @owner, context: ctx, expire_time: 1), "Lock should exists"
 
     sleep 2
 
-    assert_equal true, adquire_lock(table: TABLE, owner: @owner, context: ctx, expire_time: 1), "Lock is should not exists"
+    assert_equal true, acquire_lock(table: TABLE, owner: @owner, context: ctx, expire_time: 1), "Lock is should not exists"
 
   ensure
     delete(ctx)
@@ -52,7 +52,7 @@ class DynalockTest < Minitest::Test
     assert_equal false, refresh_lock(table: TABLE, owner: @owner, context: ctx, expire_time: 3), "Should not be possible to refresh a non existing token"
 
 
-    adquire_lock(table: TABLE, owner: @owner, context: ctx, expire_time: 1)
+    acquire_lock(table: TABLE, owner: @owner, context: ctx, expire_time: 1)
     assert_equal true, refresh_lock(table: TABLE, owner: @owner, context: ctx, expire_time: 1)
     assert_equal false, refresh_lock(table: TABLE, owner: "superman", context: ctx, expire_time: 3), "superman should not pick the lock"
     sleep 2
